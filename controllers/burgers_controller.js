f85f402f7bb2eac2654d68d2aff2c9bf1cdb8a24
ElyriaG <ElyriaG@users.noreply.github.com>
2019-06-20 01:17:13 -0500

    Burgers can get reordered but not eaten....

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,9 +29,11 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    cat.update({
-        devoured: req.body.devoured
-    }, condition, function (result) {
+    if (req.body.devoured === 0){
+    burger.update({
+        devoured: true
+    }, condition, 
+    function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
@@ -39,6 +41,19 @@ router.put("/api/burgers/:id", function (req, res) {
             res.status(200).end();
         }
     });
+} else {
+    burger.update({
+        devoured: false
+    }, condition, 
+    function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+}
 });
 
 module.exports = router;
